3e99dd38efddd4f70804c9df94f5c346e4cc0a23
Andrew <andrew.weisbeck@gmail.com>
2022-09-07 23:45:10 -0400

    Update Website
    With the latest stuff

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,15 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+          <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            <span className="bg-gradient-to-r from-teal-700 via-cyan-400 to-sky-600 bg-clip-text text-transparent">
+              The Product Engagement Sprint
+            </span>
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+          <p className="text-xl font-bold leading-7 text-gray-500 dark:text-gray-400">
+            <span className="bg-gradient-to-r from-cyan-500 via-sky-700 to-teal-400 bg-clip-text text-transparent">
+              {siteMetadata.description}
+            </span>
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
