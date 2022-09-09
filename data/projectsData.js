9355018494c7d4b975770da4bf757d0d64a37eac
Andrew <andrew.weisbeck@gmail.com>
2022-09-09 09:29:17 -0400

    Update projects
    update

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Geaux Engaging Products',
     description: `I have attended seven sprints through Section 4 so far this year and this was one that I had been really looking forward to. Check out the website and blog for my experience in the Product Engagement Sprint.`,
-    imgSrc: '/static/images/pesproject.png',
+    imgSrc: '/static/images/pesproject.jpg',
     href: 'https://geauxengagingproducts.vercel.app/',
   },
   {
@@ -10,6 +10,12 @@ const projectsData = [
     description: `I have made a website for every sprint I've done and I decided that I should probably have a central app to link all the sites together so it is easier to find them. Check out my Section 4 project website and let me know what you think!`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Storytelling Sprint Will Stor',
+    description: `This website needs to be finished`,
+    imgSrc: 'https://geauxstorytelling.netlify.app/assets/home-banner.svg',
+    href: 'https://geauxstorytelling.netlify.app/',
   },
 ]
 
